27b0b6b40d43ecdf548818e61b1c5b38a79c031b
sofianenabli <sofienne.nabli@aleia.io>
2021-10-17 00:09:49 +0200

    add products images

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,44 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography, CardActions, Button } from '@material-ui/core';
+import Head from 'next/head';
+import Image from 'next/image';
+import Layout from '../components/Layout';
+import styles from '../styles/Home.module.css';
+import data from '../utils/data';
 
 export default function Home({children}) {
+  
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product1</li>
-          <li>Product2</li>
-          <li>Product3</li>
-        </ul>
+       <Grid container spacing={3}>
+         {data.products.map((product) => {
+           return (
+            <Grid item md={4} key={product.name}>
+             <Card>
+              <CardActionArea>
+                <CardMedia
+                component="img"
+                image={product.image} 
+                title={product.name}>
+                </CardMedia>
+                <CardContent>
+                  <Typography>
+                    {product.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Typography>${product.price}</Typography>
+                <Button size="small" color="primary">
+                  Add to cart
+                </Button>
+              </CardActions>
+             </Card>
+           </Grid>
+           )
+         })}
+       </Grid>
       </div>
     </Layout>
   )
