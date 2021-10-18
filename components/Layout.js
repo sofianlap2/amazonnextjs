48749ca9464802538d1fd3d6e07bab66574f91d8
sofianenabli <sofienne.nabli@aleia.io>
2021-10-18 10:26:52 +0200

    theme material

--- components/Layout.js
+++ components/Layout.js
@@ -1,43 +1,68 @@
 import React from 'react';
 import Head from 'next/head';
 import NextLink from 'next/link'
-import {AppBar, Container, Link, Toolbar, Typography} from "@material-ui/core"
+import {AppBar, Container, CssBaseline, createTheme , Link, MuiThemeProvider, Toolbar, Typography} from "@material-ui/core"
 import useStyles from '../utils/styles';
 
 
 export default function Layout({title, description,  children}) {
 
+    const theme = createTheme({
+        typography: {
+            h1 : {
+                fontSize: '1.6rem',
+                fontWeight: 400,
+                margin: '1rem 0',
+            },
+            h2 : {
+                fontSize: '1.4rem',
+                fontWeight: 400,
+                margin: '1rem 0',
+            }
+        },
+        palette: {
+            type: 'light',
+            primary: {
+                main: '#f0f000'
+            },secondary: {
+                main: '#208080',
+            }
+        }
+    })
     const classes = useStyles();
 
     return (
         <div>
             <Head>
                 <title>{title? `${title} - Next Amazona` : 'Next Amazona'}Next Amazona</title>
-                {description && <meta name="description" content={decription}></meta>}
             </Head>
-            <AppBar className={classes.navbar} position="static">
-                <Toolbar>
-                    <NextLink href="/" passHref>
-                        <Link>
-                            <Typography className={classes.brand}>amazona</Typography>
-                        </Link>
-                    </NextLink>
-                    <div className={classes.grow}>
-                        <NextLink href="/cart" passHref>
-                            <Link>Cart</Link>
+            <MuiThemeProvider theme={theme}>
+                <CssBaseline /> 
+                <AppBar className={classes.navbar} position="static">
+                    <Toolbar>
+                        <NextLink href="/" passHref>
+                            <Link>
+                                <Typography className={classes.brand}>amazona</Typography>
+                            </Link>
                         </NextLink>
-                        <NextLink href="/login" passHref>
-                            <Link>Login</Link>
-                        </NextLink>
-                    </div>
-                </Toolbar> 
-            </AppBar>
-            <Container className={classes.main}>
-                {children}
-            </Container>
-            <footer className={classes.footer}>
-                <Typography>All rights reserved. Next Amazona</Typography>
-            </footer>
+                        <div className={classes.grow}>
+                            <NextLink href="/cart" passHref>
+                                <Link>Cart</Link>
+                            </NextLink>
+                            <NextLink href="/login" passHref>
+                                <Link>Login</Link>
+                            </NextLink>
+                        </div>
+                    </Toolbar> 
+                </AppBar>
+                <Container className={classes.main}>
+                    {children}
+                </Container>
+                <footer className={classes.footer}>
+                    <Typography>All rights reserved. Next Amazona</Typography>
+                </footer>
+            </MuiThemeProvider>
+        
         </div>
     )
 }
