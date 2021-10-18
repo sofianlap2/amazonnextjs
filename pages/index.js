45e9e864d9088632113078bfe692af6de59756c9
sofianenabli <sofienne.nabli@aleia.io>
2021-10-18 13:10:31 +0200

    get data by database

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,19 @@ import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
 import data from '../utils/data';
 import  NextLink from 'next/link';
+import db from '../utils/db';
+import Product from '../models/Product';
 
-export default function Home({children}) {
+export default function Home(props) {
+
+  const {products} = props;
   
   return (
     <Layout>
       <div>
         <h1>Products</h1>
        <Grid container spacing={3}>
-         {data.products.map((product) => {
+         {products.map((product) => {
            return (
             <Grid item md={4} key={product.name}>
              <Card>
@@ -45,4 +49,15 @@ export default function Home({children}) {
       </div>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find({}).lean();
+  await db.disconnect();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    }
+  }
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -6,12 +6,12 @@ import { Button, Grid, Link, List, ListItem, Typography } from '@material-ui/cor
 import NextLink from 'next/link';
 import useStyles from '../../utils/styles';
 import Image from 'next/image';
+import Product from '../../models/Product';
+import db from '../../utils/db';
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+    const {product} = props;
     const classes = useStyles();
-    const router = useRouter();
-    const {slug} = router.query;
-    const product = data.products.find(a=>a.slug === slug)
 
     if (!product) {
         return <div>Product not found</div>
@@ -81,3 +81,17 @@ export default function ProductScreen() {
         </Layout>
     );
 }
+
+export async function getServerSideProps(context) {
+    const {params} = context;
+    const {slug} = params;
+    
+    await db.connect();
+    const product = await Product.findOne({slug}).lean();
+    await db.disconnect();
+    return {
+      props: {
+        product : db.convertDocToObj(product),
+      }
+    }
+  }
