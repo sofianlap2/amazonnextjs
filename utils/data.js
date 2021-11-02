2e7be6def5f4246b47693036caf9ee8954ca8b39
sofianenabli <sofienne.nabli@aleia.io>
2021-11-02 11:45:47 +0100

    add user in database

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+        name: 'John',
+        email: 'admin@examplie.com',
+        password: bcrypt.hashSync('123456'),
+        isAdmin: true,
+        },{
+        name: 'Jane',
+        email: 'user@examplie.com',
+        password: bcrypt.hashSync('123456'),
+        isAdmin: false,
+        },
+
+    ],
     products: [
         {
             name: 'Free Shirt',
