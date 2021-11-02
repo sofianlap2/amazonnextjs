289730a079aea549b3e8dba4d44436824e17633d
sofianenabli <sofienne.nabli@aleia.io>
2021-11-02 12:06:58 +0100

    login token

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,29 @@
 import { Button, List, ListItem, TextField, Typography, Link } from '@material-ui/core'
-import React from 'react'
+import React, {useState} from 'react'
 import Layout from '../components/Layout'
 import useStyles from '../utils/styles'
 import NextLink from 'next/link';
+import axios from 'axios';
 
 export default function Login() {
     const classes = useStyles();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitHandler = async(e) => {
+        e.preventDefault();
+        try {
+            const {data} = await axios.post('/api/users/login', {email, password})
+            alert('success login')
+        } catch (err) {
+            alert(err.message);
+        }
+        
+    }
 
     return (
         <Layout title="Login">
-            <form >
+            <form onSubmit={submitHandler}>
                 <Typography component="h1" variant="h1">
                     Login
                 </Typography>
@@ -19,14 +33,18 @@ export default function Login() {
                         variant="outlined"
                         fullWidth id="email" 
                         label="Email"
-                        inputProps={{type: 'email'}}>
+                        inputProps={{type: 'email'}}
+                        onChange={e => setEmail(e.target.value)}
+                        >
                         </TextField>
 
                         <TextField 
                         variant="outlined"
                         fullWidth id="password" 
                         label="Password"
-                        inputProps={{type: 'password'}}>
+                        inputProps={{type: 'password'}}
+                        onChange={e => setPassword(e.target.value)}
+                        >
                         </TextField>
                     </ListItem>
                     <ListItem>
